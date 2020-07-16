0aa483a36e426f7a4c6ff599ff35f7853a5a5639
LtdJorge <jorgekq@hotmail.es>
2020-07-16 22:47:00 +0200

    Basic queue functionality

--- index.js
+++ index.js
@@ -1,19 +1,31 @@
 const Net = require('net');
+const RedisMQ = require('rsmq-promise');
 
 const port = 9100;
 const config = require('./config.json');
+const queueName = 'PrinterQueue';
 
 const server = new Net.Server();
 const client = new Net.Socket();
+const queue = new RedisMQ({options: {
+        path: '/var/run/redis/redis-server.sock'
+    }});
 
+//Redis queue
+queue.createQueue({
+    qname: queueName
+}).then(done => console.log('[Servidor]> Queue created')).catch(error => console.log(error));
+
+//TCP server
 server.listen(port, () => console.log(`Server listening on port ${port}`));
+
 server.on('connection', socket => {
 
-    console.log('> Servidor conectado');
-
-    client.connect(config.printers[0].port, config.printers[0].address, () => {
-        console.log('Client connected')
-    });
+    console.log('[Servidor]> Servidor conectado');
+    //TODO: move
+    /*client.connect(config.printers[0].port, config.printers[0].address, () => {
+        console.log('[Cliente]> Cliente conectado')
+    });*/
     
     let chunkBuffer = [];
 
@@ -23,31 +35,25 @@ server.on('connection', socket => {
         chunkNumber++
     });
     socket.on('end', () => {
-        console.log('> Servidor desconectado');
-        console.log(`> Recibidos: ${chunkNumber} chunks`);
-        console.log('> Enviando a impresora');
-        chunkBuffer.forEach(chunk => {
+        console.log('[Servidor]> Servidor desconectado');
+        console.log(`[Servidor]> Recibidos: ${chunkNumber} chunks`);
+        console.log('[Servidor]> Enviando a impresora');
+        let message = {
+            printer: config.printer,
+            printJob: chunkBuffer
+        }
+        console.log(`[Servidor]> ${JSON.stringify(message)}`);
+        /*queue.sendMessage({
+
+        })*/
+        /*chunkBuffer.forEach(chunk => {
                 await writeAsync(chunk, client);
-        });
+        });*/
     });
-    socket.on('error', err => console.error(`Error: ${err}`));
+    socket.on('error', err => console.error(`[Servidor]> Error: ${err}`));
 })
 
-async function writeAsync(data, client){
-    client.write(data);
-    
-}
-/*
-async function sendToPrinter(buffer, allPrinters){
-    if(allPrinters){
-        config.printers.forEach(printer => {
-            rawPrint(buffer, printer.numberOfCopies)
-        });
-    }
-}
-
-function rawPrint(buffer, times){
-    for(i=0; i<times; i++){
-        printer.printText(buffer, {}, )
-    }
-}*/
+process.on('exit', () => {
+    console.log('Exiting...');
+    queue.deleteQueue({qname: queueName});
+})
