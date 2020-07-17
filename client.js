aa732f64c9dfa6366d8fd3565d8085a0770e9bb2
LtdJorge <jorgekq@hotmail.es>
2020-07-17 19:52:40 +0200

    Changed the client to respond to evented messages instead of polling

--- client.js
+++ client.js
@@ -1,15 +1,20 @@
 const Net = require('net');
 const RedisMQ = require('rsmq');
+const RSMQWorker = require('rsmq-worker');
 
 const port = 9100;
 const config = require('./config.json');
 const queueName = 'PrinterQueue';
 
 const client = new Net.Socket();
+const worker = new RSMQWorker(queueName);
 
+/*
 const queue = new RedisMQ({options: {
     path: '/var/run/redis/redis-server.sock'
 }});
+ */
+
 
 client.on('connect', () => console.log('Connected'));
 client.on('error', (err) => console.log(err));
@@ -18,8 +23,18 @@ client.on('error', (err) => console.log(err));
 client.connect(8100, '127.0.0.1',
     () => {
         console.log('[Cliente]> Cliente conectado');
+        worker.start()
     });
 
+worker.on('message', (message, next, id) => {
+    console.log(`[Cliente]> Borrando mensaje con id: ${message.id}`);
+    const buff = Buffer.from(JSON.parse(message).printJob(), 'latin1');
+    client.write(buff);
+    worker.del(id);
+    next();
+});
+
+/*
 function loop(){
     while(true){
         queue.receiveMessage({qname: queueName})
@@ -32,6 +47,7 @@ function loop(){
         .catch((err) => console.log(err));
     }
 }
+ */
 
 let q  = queue.createQueue({qname: queueName}).then();
 
