913f1443d5bb3bf645c148759f834ad46268618e
LtdJorge <jorgekq@hotmail.es>
2020-07-20 21:37:08 +0200

    pm2 autostartup init

---
+++ autostart.js
@@ -0,0 +1,24 @@
+const pm2 = require('pm2');
+
+pm2.connect(err => {
+    if (err) console.log(err);
+    process.exit(2);
+});
+
+pm2.start({
+    name: 'fake-printer',
+    script: './index.js',
+    output: './logs/output.log',
+    error: './logs/error.log',
+    pid: './fake-printer.pid',
+    max_restarts: 5,
+    interpreter: 'node',
+    watch: true
+    }, (err, proc) =>{
+        console.error(err);
+});
+
+pm2.startup('systemd', ((err, result) => {
+    console.error(err);
+    pm2.disconnect();
+}));
--- index.js
+++ index.js
@@ -2,8 +2,6 @@ const Net = require('net');
 
 const port = 9100;
 const config = require('./config.json');
-const { Console } = require('console');
-const queueName = 'PrinterQueue';
 
 const server = new Net.Server();
 
@@ -16,39 +14,25 @@ server.on('connection', socket => {
     remoteSocket1.connect(config.printer.port, config.printer.address);
     remoteSocket2.connect(config.printer.port, config.printer.address);
     remoteSocket3.connect(config.printer.port, config.printer.address);
-    
 
     socket.on('connect', ()=> {
         console.log('[Servidor]> Servidor conectado');
     });
 
-    let count = 0;
-    const buffEnd = new Buffer.from([0x1B,0x40,0x1D,0x56,0x1]);
-
     socket.on('data', chunk => {
-        /* if(!count){
-            //console.log('[Servidor]> Enviando ' + chunk.toString("latin1"));
-            //let data = Buffer.concat([chunk, buffEnd]);*/
-            
-             remoteSocket1.write(chunk, () => {
+         remoteSocket1.write(chunk, () => {
+            remoteSocket1.write(chunk, () => {
                 remoteSocket1.write(chunk, () => {
-                    remoteSocket1.write(chunk, () => {
-                        console.log('[Servidor]> Enviadas 3 copias');
-                    });
+                    console.log('[Servidor]> Enviadas 3 copias');
                 });
-            });            
-            /*count++;
-        } else{
-            count = 0;
-        } */
+            });
+        });
     });
     /*****/socket.on('error', err => console.error(`[Servidor]> SServer error: ${err}`));
     remoteSocket1.on('error', err => console.error(`[Servidor]> Socket1 error: ${err}`));
     remoteSocket2.on('error', err => console.error(`[Servidor]> Socket2 error: ${err}`));
     remoteSocket3.on('error', err => console.error(`[Servidor]> Socket3 error: ${err}`));
-})
-
-
+});
 
 //TCP server
 server.listen(port, () => console.log(`[Servidor]> Escuchando en puerto: ${port}`));
